9c0c43158ab32e9fdca649008930b5049f201887
David Corticchiato <david.corticchiato@gmail.com>
2025-08-09 15:49:47 -0400

    workaround: freeze body for 100 ms

--- controllers/picker/picker.gd
+++ controllers/picker/picker.gd
@@ -1,4 +1,4 @@
-extends CharacterBody3D
+extends Node3D
 
 const RAY_LENGTH = 1000
 const PULL_VELOCITY = 2
@@ -8,16 +8,32 @@ class FocusedObject:
     var object: RigidBody3D
     var dragging: bool
     var distance_to_contact: float
+    var normal: Vector3
+    var pause_time_left: float
 
     func _init(result: Dictionary, camera: Camera3D):
         contact = result['position']
         object = result['collider']
+        normal = result['normal'].normalized()
         dragging = false
         distance_to_contact = camera.global_position.distance_to(contact)
 
+    func pause():
+        (object as RigidBody3D).set_deferred('freeze', true)
+        pause_time_left = 0.1
+
+    func update(delta: float):
+        if pause_time_left > 0:
+            pause_time_left -= delta
+            if pause_time_left <= 0:
+                (object as RigidBody3D).set_deferred('freeze', false)
+
+
+
 @onready var _camera: Camera3D = get_viewport().get_camera_3d()
-@onready var _joint: PinJoint3D = $PinJoint3D
-@onready var _drag_wall: Area3D = $DragWall
+@onready var _joint: Generic6DOFJoint3D = $Joint
+@onready var _drag_wall: Area3D = $Hand/DragWall
+@onready var _hand: CharacterBody3D = $Hand
 
 var _focused_object: FocusedObject = null
 
@@ -37,11 +53,9 @@ func _physics_process(delta):
         _focused_object = FocusedObject.new(result, _camera) if result.size() > 0 else null
     
     if _focused_object != null and _focused_object.dragging:
-        
-        global_position = _camera.global_position + _camera.global_position.direction_to(global_position) * _focused_object.distance_to_contact
+        _focused_object.update(delta)
+        _hand.global_position = _camera.global_position + _camera.global_position.direction_to(_hand.global_position) * _focused_object.distance_to_contact
 
-        
-        #_drag_wall.global_position.y = _camera.global_position.y
         _drag_wall.global_rotation.y = _camera.global_rotation.y
         var query = PhysicsRayQueryParameters3D.create(origin, end)
         query.collide_with_areas = true
@@ -50,18 +64,19 @@ func _physics_process(delta):
         var result: Dictionary = space_state.intersect_ray(query)
         
         if result.size() > 0:
-            global_position = _camera.global_position + _camera.global_position.direction_to(result['position']) * _camera.global_position.distance_to(global_position)
-            
+            _hand.global_position = _camera.global_position + _camera.global_position.direction_to(result['position']) * _camera.global_position.distance_to(_hand.global_position)
             
     
 
 func _process(delta):
     if _focused_object != null:
         if not _focused_object.dragging and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-            global_position = _focused_object.contact
+            _hand.global_position = _focused_object.contact
+            _joint.global_position = _focused_object.contact
             _focused_object.dragging = true
-            _joint.set_node_a(self.get_path())
+            _joint.set_node_a(_hand.get_path())
             _joint.set_node_b(_focused_object.object.get_path())
+            _focused_object.pause()
         elif _focused_object.dragging and not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
             _focused_object.dragging = false
             _joint.set_node_a(NodePath(""))
