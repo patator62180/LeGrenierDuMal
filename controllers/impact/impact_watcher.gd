2687fc239f46503e79309b43ec81d9b41ae722b8
David Corticchiato <david.corticchiato@gmail.com>
2025-08-09 16:59:49 -0400

    sound gauge

--- controllers/impact/impact_watcher.gd
+++ controllers/impact/impact_watcher.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var _curve: Curve
+
 var _rigid_body: RigidBody3D
 
 func _ready():
@@ -12,8 +14,16 @@ func _ready():
         printerr("ImpactWatcher is deactivated because it's not under a rigid body")
         set_process(false)
         set_physics_process(false)
-    else:
+
+    if _curve == null:
+        printerr("ImpactWatcher is deactivated because curve hasn't been set")
+        set_process(false)
+        set_physics_process(false)
+    
+    if _rigid_body != null and _curve != null:
         _rigid_body.body_entered.connect(_on_body_entered)
+        _rigid_body.contact_monitor = true
+        _rigid_body.max_contacts_reported = 5
 
 func _on_body_entered(body: Node):
-    Game.Controller.instance.add_impact(_rigid_body.mass, _rigid_body.linear_velocity.length(), 0.5)
+    Game.Controller.instance.add_impact(_curve, _rigid_body.linear_velocity.length())
