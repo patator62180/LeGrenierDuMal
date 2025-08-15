9fb92b12b1ea69e6a53c45c740f225e7e62891c2
Samuel Terras-Viola <samuel.terras-viola@ubisoft.com>
2025-08-15 16:56:34 -0400

    replacing fog effect with vignette when you make noise

--- controllers/hud/hud.gd
+++ controllers/hud/hud.gd
@@ -2,14 +2,17 @@ extends Control
 
 @onready var _sound_gauge_value: ColorRect = $SoundGauge/Value
 @onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _vignette : CanvasItem = $Vignette
 
 func _ready():
-    Game.Controller.instance.cumulated_sound_changed.connect(_on_cumulated_sound_changed)
-    Game.Controller.instance.loading_completed.connect(_on_loading_complete)
-    _animation_player.play("loading")
+	Game.Controller.instance.cumulated_sound_changed.connect(_on_cumulated_sound_changed)
+	Game.Controller.instance.loading_completed.connect(_on_loading_complete)
+	_animation_player.play("loading")
 
 func _on_loading_complete():
-    _animation_player.play("close_loading")
+	_animation_player.play("close_loading")
 
 func _on_cumulated_sound_changed(sound: float):
-    _sound_gauge_value.scale.x = min(Game.MAX_SOUND_VALUE, sound) / Game.MAX_SOUND_VALUE
+	var relative_sound = min(Game.MAX_SOUND_VALUE, sound) / Game.MAX_SOUND_VALUE
+	_sound_gauge_value.scale.x = relative_sound
+	_vignette.material.set_shader_parameter("inner_radius", 1.0-relative_sound)
