951d7c8c9b472ce0ca9b4643b8cd0fda30d6a3fb
David Corticchiato <david.corticchiato@gmail.com>
2025-08-14 21:50:38 -0400

    apply curve to sound

--- controllers/game/game.gd
+++ controllers/game/game.gd
@@ -5,7 +5,7 @@ const MAX_VELOCITY: float = 3
 const MIN_VELOCITY: float = 0.001
 
 const MAX_SOUND_VALUE: float = 1.25
-const HIGH_MASS: float = 5
+const HIGH_MASS: float = 30
 const LOADING_PERIOD: float = 2
 const LOADING_PERIOD_IMPACTS_THRESHOLD = 80
 
@@ -14,12 +14,14 @@ class Impact:
     var velocity: float
     var time_left: float
     var mass: float
+    var audio_player: AudioStreamPlayer3D
     
-    func _init(curve: Curve, velocity: float, mass: float):
+    func _init(curve: Curve, velocity: float, mass: float, audio_player: AudioStreamPlayer3D):
         self.curve = curve
         self.velocity = velocity
         self.time_left = 1
         self.mass = mass
+        self.audio_player = audio_player
 
 class Controller:
     #region singleton
@@ -38,8 +40,8 @@ class Controller:
     var _loading_period_finished: bool = false
     var _loading_period_counter: float = 0
     
-    func add_impact(curve: Curve, velocity: float, mass: float):
-        _impacts.push_back(Impact.new(curve, velocity, mass))
+    func add_impact(curve: Curve, velocity: float, mass: float, audio_player: AudioStreamPlayer3D):
+        _impacts.push_back(Impact.new(curve, velocity, mass, audio_player))
 
     func update(delta: float):
         if not _loading_period_finished:
@@ -56,7 +58,11 @@ class Controller:
             
             for impact in _impacts:
                 var velocity_ratio = (max(min(impact.velocity, MAX_VELOCITY), MIN_VELOCITY) - MIN_VELOCITY) / (MAX_VELOCITY - MIN_VELOCITY)
-                cumulated_sound += impact.curve.sample(1.0 - impact.time_left) * velocity_ratio * impact.mass / HIGH_MASS
+                var velocity_curve_sample = impact.curve.sample(1.0 - impact.time_left) * velocity_ratio
+                
+                impact.audio_player.volume_linear = remap(velocity_curve_sample, 0, 2, 0, 1)
+                
+                cumulated_sound += velocity_curve_sample * impact.mass / HIGH_MASS
                 
             cumulated_sound_changed.emit(cumulated_sound)
             
